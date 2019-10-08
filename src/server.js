1da7041ad6450072bd61971eeceb0eb6720b0817
Ronkzinho Extreme <henriquefrcr@gmail.com>
2019-10-08 19:10:54 -0300

    First backend

--- src/server.js
+++ src/server.js
@@ -9,10 +9,7 @@ const server = http.Server(app)
 const bodyParser = require('body-parser')
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-app.use((req, res) => {
-  req.header("Access-Control-Allow-Origin", "*")
-  return next()
-})
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
