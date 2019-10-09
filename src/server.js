22afd079bc30ea8a9de7d50a1ba7093f41820a0a
Ronkzinho Extreme <henriquefrcr@gmail.com>
2019-10-08 22:42:55 -0300

    First frotend

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 app.use((req, res, next) => {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
