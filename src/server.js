d9d135b65f75fa8011ead50c4659836137ccc487
Ronkzinho Extreme <henriquefrcr@gmail.com>
2019-10-21 19:11:34 -0300

    First backend

--- src/server.js
+++ src/server.js
@@ -3,13 +3,14 @@ const express = require("express")
 const cors = require("cors")
 const http = require("http")
 const mongoose = require("mongoose")
+const ip = require("ip")
 const routes = require("./routes")
 const app = express()
 const server = http.Server(app)
 const bodyParser = require('body-parser')
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: [`http://localhost:3000`, "https://saldo-do-ronk.herokuapp.com", null] }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
